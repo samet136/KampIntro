842fb095cb1a99d564bebe8bc7b7c4067beecb04
samet136 <sametbayram459@gmail.com>
2021-07-12 18:15:31 +0300

    12.07.2021

---
+++ OOP2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "123456";
+            musteri1.Adi = "Samet";
+            musteri1.Soyadi = "Bayram";
+            musteri1.TcNo = "11111111111";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 1;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "GameR";
+            musteri2.VergiNo = "1234567890";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() { new SmsLoggerManager(), new FileLoggerService() };
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+
+
+
+
+           
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager,konutKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            Console.ReadKey();
+
+        }
+    }
+}
